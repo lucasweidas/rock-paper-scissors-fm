e9492fc2057876277bc9410ad0765f4c1967ed35
lucasweidas <lucasweidas@gmail.com>
2023-10-01 19:06:25 -0300

    add base tailwind classes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import { Barlow_Semi_Condensed } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: 'Frontend Mentor | Rock, Paper, Scissors',
-  description: 'Play the classic rock, paper, scissors online.',
+  description: 'Play the classic rock, paper, scissors online for free.',
 };
+
+const barlowSemiCondensed = Barlow_Semi_Condensed({
+  subsets: ['latin'],
+  weight: ['600', '700'],
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${barlowSemiCondensed.className}`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,27 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          500: 'hsl(217, 16%, 45%)',
+          700: 'hsl(229, 25%, 31%)',
+        },
+        blue: {
+          500: 'hsl(229, 64%, 46%)',
+        },
+      },
+      backgroundImage: {
+        'linear-1':
+          'linear-gradient(90deg, hsl(39, 89%, 49%), hsl(40, 84%, 53%))',
+        'linear-2':
+          'linear-gradient(90deg, hsl(230, 89%, 62%), hsl(230, 89%, 65%))',
+        'linear-3':
+          'linear-gradient(90deg, hsl(349, 71%, 52%), hsl(349, 70%, 56%))',
+        'radial-1':
+          'radial-gradient(circle at 50% 0%, hsl(214, 47%, 23%), hsl(237, 49%, 15%))',
+      },
+    },
   },
   plugins: [],
 };

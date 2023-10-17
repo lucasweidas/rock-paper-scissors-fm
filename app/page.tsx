b660b80ff5e740d0dbdedba35096a2eb3a281a41
lucasweidas <lucasweidas@gmail.com>
2023-10-17 18:40:19 -0300

    finish TopBar large version

--- app/page.tsx
+++ app/page.tsx
@@ -43,24 +43,7 @@ export default function Home() {
         Choose one of the options below to start playing.
       </h1>
       <div className="px-7 pt-8 pb-14 min-h-screen flex flex-col">
-        <div className="flex gap-4 justify-between items-center py-3 pl-5 pr-3 rounded-lg shadow-[0_0_2px_2px_hsl(217,16%,45%),inset_0_0_2px_2px_hsl(217,16%,45%)] max-w-md mx-auto w-full">
-          <div className="w-[82px] h-[51px] relative flex-shrink-0">
-            <Image
-              src={logo}
-              alt="Rock, Paper, Scissors"
-              fill
-              className="object-cover"
-            />
-          </div>
-          <div className="w-20 h-[4.5rem] p-3 flex items-center justify-center flex-col bg-white rounded-lg">
-            <h2 className="text-blue-500 font-semibold uppercase text-xs tracking-widest">
-              Score
-            </h2>
-            <span className="text-gray-700 text-[2rem] !leading-none font-bold uppercase">
-              {score}
-            </span>
-          </div>
-        </div>
+        <TopBar score={score} />
         <div className="max-w-xs mx-auto mt-24 w-full">
           <AnimatePresence initial={false}>
             {playerPick ? (
@@ -75,9 +58,33 @@ export default function Home() {
               <Picking onPick={handlePlayerPick} />
             )}
           </AnimatePresence>
+          T
         </div>
         <RulesWrapper />
       </div>
     </main>
+  );
+}
+
+function TopBar({ score }: { score: number }) {
+  return (
+    <div className="flex gap-4 justify-between items-center py-3 pl-5 pr-3 md:py-4 md:pl-8 md:pr-6 rounded-lg shadow-[0_0_2px_2px_hsl(217,16%,45%),inset_0_0_2px_2px_hsl(217,16%,45%)] max-w-md mx-auto w-full md:max-w-[44rem]">
+      <div className="w-[82px] h-[51px] md:w-[162px] md:h-[99px] relative flex-shrink-0">
+        <Image
+          src={logo}
+          alt="Rock, Paper, Scissors"
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className="min-w-20 h-[4.5rem] md:min-w-[9.5rem] md:h-28 p-3 md:px-4 flex items-center justify-center flex-col bg-white rounded-lg">
+        <h2 className="text-blue-500 font-semibold uppercase text-xs tracking-widest md:text-base !leading-none">
+          Score
+        </h2>
+        <span className="text-gray-700 text-[2rem] !leading-none font-bold uppercase md:text-6xl">
+          {score}
+        </span>
+      </div>
+    </div>
   );
 }

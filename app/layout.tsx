75f61d0954979cf080f484a8066fa78f37f00311
lucasweidas <lucasweidas@gmail.com>
2023-10-19 19:30:26 -0300

    change fonts from google to local

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,25 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Barlow_Semi_Condensed } from 'next/font/google';
+import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
   title: 'Frontend Mentor | Rock, Paper, Scissors',
   description: 'Play the classic rock, paper, scissors online for free.',
 };
 
-const barlowSemiCondensed = Barlow_Semi_Condensed({
-  subsets: ['latin'],
-  weight: ['600', '700'],
+const barlowSemiCondensed = localFont({
+  src: [
+    {
+      path: './fonts/BarlowSemiCondensed-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/BarlowSemiCondensed-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
   variable: '--font-barlow-semi-condensed',
 });
 
